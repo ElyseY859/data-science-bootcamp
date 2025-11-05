a0da503603a39f3aea515880666e13f3674c42d2
ElyseY859 <elyseyan859@gmail.com>
2025-11-05 12:09:26 -0500

    Add files via upload

---
+++ week5.py
@@ -0,0 +1,5 @@
+#pandas
+import pandas as pd
+
+def combine_two_tables(person: pd.DataFrame, address: pd.DataFrame) -> pd.DataFrame:
+    return person.merge(address, how='left', on='personId')[['firstName', 'lastName', 'city', 'state']]
